79f6ac88a85c2acb4afa411bb3a25fe903173156
renan luz <humrenan@gmail.com>
2022-06-12 20:40:59 -0400

    style: format

--- fibonacci.erl
+++ fibonacci.erl
@@ -7,8 +7,6 @@ fib(2) -> 1;
 fib(N) when N>2 ->
     fib(N-1) + fib(N-2).
 
-%    depois = erlang:system_time().
-%    total = erlang:now_diff(antes, depois).
 fib_with_history(N) ->
     fib_with_history(N,1,1,0).
 
@@ -17,8 +15,6 @@ fib_with_history(2,_,_,_) -> 1;
 fib_with_history(N,N,P1,_P2) when N>2 -> P1;
 fib_with_history(N,M,P1,P2) when N>2 -> 
     fib_with_history(N,M+1,P2+P1,P1).
-
-
 
 test() ->
     true = (fib(1)==1),
